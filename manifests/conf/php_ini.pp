57b4aece741f3747ab0a4169b357c992029fddcc
s1rc0 <sergey.postument@gmil.com>
2014-12-17 23:06:00 +0200

    add php.ini template

---
+++ manifests/conf/php_ini.pp
@@ -0,0 +1,20 @@
+class phpfpm::conf::php_ini (
+  $max_execution_time             = $phpfpm::max_execution_time,
+  $max_input_time                 = $phpfpm::max_input_time,
+  $memory_limit                   = $phpfpm::memory_limit,
+  $file_uploads                   = $phpfpm::file_uploads,
+  $upload_max_filesize            = $phpfpm::upload_max_filesize,
+  $max_file_uploads               = $phpfpm::max_file_uploads,
+  $date_timezone                  = $phpfpm::date_timezone,
+) {
+
+  file {"/etc/php.ini":
+    content => template('phpfpm/php54.ini.erb'),
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Class['phpfpm::install'],
+    notify => Class['phpfpm::service'],
+  }
+
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,20 @@ class phpfpm (
   $fastcgi_type_socket  = '/var/run/php-fpm.sock',
   $php_modules          = [],
 
+  # php.ini configuration
+  $max_execution_time             = '30',
+  $max_input_time                 = '60',
+  $memory_limit                   = '128M',
+  $file_uploads                   = "On",
+  $upload_max_filesize            = "2M",
+  $max_file_uploads               = 20,
+  $date_timezone                  = "Europe/Kiev",
+
 ){
   include phpfpm::install
   include phpfpm::service
 
+  /**
   file {"/etc/php.ini":
     content => template('phpfpm/php54.ini.erb'),
     owner => 'root',
@@ -19,4 +29,5 @@ class phpfpm (
     require => Class['phpfpm::install'],
     notify => Class['phpfpm::service'],
   }
+  **/
 }
