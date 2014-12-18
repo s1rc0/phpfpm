d2a18748b6b0e7db2ad540fe3f453ca2c9d94b60
s1rc0 <sergey.postument@gmil.com>
2014-12-18 19:02:42 +0200

    add configuration www.conf

--- manifests/conf/php_fpm.pp
+++ manifests/conf/php_fpm.pp
@@ -11,15 +11,6 @@ class phpfpm::conf::php_fpm (
   $pm_max_spare_servers       = 35,
   $slowlog                    = '/var/log/php-fpm/www-slow.log',
 
-  /**
-  $max_execution_time             = $phpfpm::max_execution_time,
-  $max_input_time                 = $phpfpm::max_input_time,
-  $memory_limit                   = $phpfpm::memory_limit,
-  $file_uploads                   = $phpfpm::file_uploads,
-  $upload_max_filesize            = $phpfpm::upload_max_filesize,
-  $max_file_uploads               = $phpfpm::max_file_uploads,
-  $date_timezone                  = $phpfpm::date_timezone,
-  */
 ) {
 
   file {"/etc/php-fpm.d/www.conf":
