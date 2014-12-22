ec84f1b6ff8cf3b122e37dda7ced81e224549b22
s1rc0 <sergey.postument@gmil.com>
2014-12-22 17:43:48 +0200

    v.0.2.0a

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,16 +6,12 @@ class phpfpm (
   require phpfpm::install
   require phpfpm::service
 
-  if ::phpfpm::conf::php_ini::memory_limit == undef
-  {
-    $max_execution_time             = '30'
-    $max_input_time                 = '60'
-    $memory_limit                   = '128M'
-    $file_uploads                   = 'On'
-    $upload_max_filesize            = '2M'
-    $max_file_uploads               = 20
-    $date_timezone                  = "Europe/Kiev"
-    include phpfpm::conf::php_ini
+  if !defined(Class['phpfpm::conf::php_ini']) {
+    class { '::phpfpm::conf::php_ini':
+      max_execution_time        => 30,
+      max_input_time          => 60,
+      memory_limit          => '128M',
+    }
   }
 /*
   file { "/etc/php-fpm.d":
