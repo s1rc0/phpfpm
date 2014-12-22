fcab48a1fd96894d3db9d7d5f76d5be8dfa4e39f
s1rc0 <sergey.postument@gmil.com>
2014-12-22 17:19:21 +0200

    v.0.2.0a

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,15 @@ class phpfpm (
   require phpfpm::install
   require phpfpm::service
 
-  if phpfpm::conf::php_ini::memory_limit != undef
+  if !phpfpm::conf::php_ini::memory_limit
   {
+    $phpfpm::conf::php_ini::max_execution_time             = '30'
+    $phpfpm::conf::php_ini::max_input_time                 = '60'
+    $phpfpm::conf::php_ini::memory_limit                   = '128M'
+    $phpfpm::conf::php_ini::file_uploads                   = 'On'
+    $phpfpm::conf::php_ini::upload_max_filesize            = '2M'
+    $phpfpm::conf::php_ini::max_file_uploads               = 20
+    $phpfpm::conf::php_ini::date_timezone                  = "Europe/Kiev"
     include phpfpm::conf::php_ini
   }
 /*
