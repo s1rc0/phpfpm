43ce4bf5622efcc613b19d112ea5b52e2caa403a
s1rc0 <sergey.postument@gmil.com>
2014-12-22 17:06:42 +0200

    v.0.2.0a

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@ class phpfpm (
   require phpfpm::service
 
   if !$phpfpm::conf::php_ini::memory_limit or
-  !$phpfpm::conf::php_ini::memory_limit or
+  !$phpfpm::conf::php_ini::max_execution_time or
   !$phpfpm::conf::php_ini::highlight_keyword
   {
     include phpfpm::conf::php_ini
