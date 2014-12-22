179dd97cfd8c43531496e81f0619654841b95187
s1rc0 <sergey.postument@gmil.com>
2014-12-22 21:00:17 +0200

    v.0.2.0a

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,8 @@ class phpfpm (
   $php_modules          = [],
 ){
   include phpfpm::install
-  if defined("phpfpm::conf::php_ini") {
-    notify (Class['phpfpm::service'])
-    include phpfpm::conf::php_ini
-  }
+  include phpfpm::conf::php_ini
   include phpfpm::service
-
-
 /*
   file { "/etc/php-fpm.d":
     ensure  => directory,
